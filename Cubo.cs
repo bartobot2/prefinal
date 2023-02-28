40ba237bdca50e42d7a6f8f9dbc70f8b759fb55e
bartobot2 <126626438+bartobot2@users.noreply.github.com>
2023-02-28 20:07:40 -0300

    Add files via upload

---
+++ Cubo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Cubo
+{
+	public Cubo()
+	{
+		double arista;
+		int color;
+		int trazo;
+
+		public Cubo()
+        {
+
+        }
+
+		public double GetArea()
+        {
+			return 6 * Math.Pow(arista, 2);
+        }
+
+
+		public double GetVolumen()
+		{ 
+		    return Math.Pow(arista, 3);
+		}
+
+
+
+
+	}
+}
